43af771a8fb7b8a0de2333bb72bbea25615d4738
Mertcan Mermerkaya <mertcan@mermerkaya.net>
2016-03-01 20:27:33 +0200

    Improved 1.3 (URLify)
    Changed the solution to an in-place one. Fixed test strings.

--- Chapter 1/3_URLify/URLify.py
+++ Chapter 1/3_URLify/URLify.py
@@ -1,25 +1,31 @@
 # O(N)
 import unittest
 
+
 def urlify(string, length):
     '''function replaces single spaces with %20 and removes trailing spaces'''
-    counter = 0
-    output = ''
-    for char in string:
-        counter += 1
-        if counter > length:
-            return output
-        elif char == ' ':
-            output = output + '%20'
-        elif char != ' ':
-            output = output + char
-    return output
+    new_index = len(string)
+
+    for i in reversed(range(length)):
+        if string[i] == ' ':
+            # Replace spaces
+            string[new_index - 3:new_index] = '%20'
+            new_index -= 3
+        else:
+            # Move characters
+            string[new_index - 1] = string[i]
+            new_index -= 1
+
+    return string
+
 
 class Test(unittest.TestCase):
     '''Test Cases'''
+    # Using lists because Python strings are immutable
     data = [
-        ('much ado about nothing          ', 22, 'much%20ado%20about%20nothing'),
-        ('Mr John Smith       ', 13, 'Mr%20John%20Smith')]
+        (list('much ado about nothing      '), 22,
+         list('much%20ado%20about%20nothing')),
+        (list('Mr John Smith    '), 13, list('Mr%20John%20Smith'))]
 
     def test_urlify(self):
         for [test_string, length, expected] in self.data:

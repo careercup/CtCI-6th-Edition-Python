25c9f6a6cc421d93ea50a8db7e29539b4e4d1f6e
Phivos Stylianides <stphivos@gmail.com>
2016-07-05 13:03:08 +0200

    Add recursive & iterative solution to chapter 8 question 4

---
+++ Chapter8/4_PowerSet/iterative.py
@@ -0,0 +1,25 @@
+from __future__ import print_function
+
+
+def subsets(arr):
+    result = [()]
+
+    for i in range(-1, len(arr) - 1):
+        for j in range(i + 1, len(arr)):
+            t = []
+            if i >= 0:
+                t.append(arr[i])
+            t.append(arr[j])
+
+            result.append(tuple(t))
+
+    result.append(tuple(arr))
+    return result
+
+
+if __name__ == '__main__':
+    q = [1, 2, 3]
+    a = subsets(q)
+
+    print('{} subsets:'.format(len(a)))
+    print(a)
---
+++ Chapter8/4_PowerSet/recursive.py
@@ -0,0 +1,25 @@
+from __future__ import print_function
+
+
+def subsets(arr):
+    if len(arr) == 1:
+        return [(), (arr[0],)]
+
+    result = []
+    n = arr[-1]
+
+    prev = subsets(arr[:-1])
+    result.extend(prev)
+
+    for s in prev:
+        result.append(tuple(list(s) + [n]))
+
+    return result
+
+
+if __name__ == '__main__':
+    q = [1, 2, 3]
+    a = subsets(q)
+
+    print('{} subsets:'.format(len(a)))
+    print(a)

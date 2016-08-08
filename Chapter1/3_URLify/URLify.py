371c31fa87a94ef1e1cec9c1a2af0abc70d32828
zhuyuecai <zhuyuecai@gmail.com>
2016-08-08 12:12:20 -0400

    clean chapter names and fix issue in c1q3

--- Chapter1/3_URLify/URLify.py
+++ Chapter1/3_URLify/URLify.py
@@ -1,10 +1,23 @@
 # O(N)
 import unittest
 
+#you need 1 extra pass to get the right starting point, since the given string may contain more 
+#spaces than needed
+
+def add_space(n_space):
+    return n_space+1
 
 def urlify(string, length):
     '''function replaces single spaces with %20 and removes trailing spaces'''
-    new_index = len(string)
+    #first pass to count the number of space in the true string
+    
+    n_space = 0
+    for i in range(length): 
+        if string[i] == ' ':
+            n_space+=1
+    
+    
+    new_index = length + 2*n_space
 
     for i in reversed(range(length)):
         if string[i] == ' ':
@@ -15,7 +28,7 @@ def urlify(string, length):
             # Move characters
             string[new_index - 1] = string[i]
             new_index -= 1
-
+   
     return string
 
 
@@ -23,8 +36,8 @@ class Test(unittest.TestCase):
     '''Test Cases'''
     # Using lists because Python strings are immutable
     data = [
-        (list('much ado about nothing      '), 22,
-         list('much%20ado%20about%20nothing')),
+        (list('much ado about nothing           '), 22,
+         list('much%20ado%20about%20nothing     ')),
         (list('Mr John Smith    '), 13, list('Mr%20John%20Smith'))]
 
     def test_urlify(self):
@@ -34,3 +47,9 @@ class Test(unittest.TestCase):
 
 if __name__ == "__main__":
     unittest.main()
+    """
+    data = [
+        (list('much ado about nothing           '), 22,
+         list('much%20ado%20about%20nothing')),
+        (list('Mr John Smith    '), 13, list('Mr%20John%20Smith'))]
+    urlify(data[0][0],data[0][1])"""

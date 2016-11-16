c32a43a7d9245263f9344189cd3e29d0ebccb128
omerzk <omeryom@gmail.com>
2016-11-16 17:32:04 +0200

    use Counter's functionality
    instead of updating the Counter manually I used Counter's ctor that accepts an iterable and counts its items.
    I also used the Counter equality operator.
    still O(n) (assuming Counter ctor is O(n) )

--- Chapter01/2_Check Permutation/CheckPermutation.py
+++ Chapter01/2_Check Permutation/CheckPermutation.py
@@ -16,6 +16,13 @@ def check_permutation(str1, str2):
     return True
 
 
+def check_permutation_pythonic(str1, str2):
+    if len(str1) != len(str2):
+        return False
+
+    return Counter(str1) == Counter(str2)
+
+
 class Test(unittest.TestCase):
     dataT = (
         ('abcd', 'bacd'),

e988c02afd788f53ef3575e95fb9acdfd103e777
Bartek Brak <bartek.rychlicki@gmail.com>
2016-12-12 12:29:50 +0100

    Add alternative implementation to 1.5 "One Away"
    I'm not entirely the author of this implementation. I knew you cand o
    better than the code in the file so I looked and found the one in CPP
    repository. I undesrtood it and added comments for others to enjoy.

--- Chapter 1/5_One Away/OneAway.py
+++ Chapter 1/5_One Away/OneAway.py
@@ -38,6 +38,51 @@ def one_edit_insert(s1, s2):
     return True
 
 
+def one_away__alternative(s1, s2):
+    """An alternative to the `one_away` above.
+    Based on the CPP approach in https://github.com/careercup/CtCI-6th-Edition-Python/blob/master/Chapter%201/5_One%20Away/OneAway.py
+
+    Iterate over both strings checking for equality.
+
+    When a first mismatch occurs:
+    * if strings are of equal lengths, assume replacement
+    * if one is shorter, delay its pointer
+    Fail on second mismatch.
+
+    """
+    l1 = len(s1)
+    l2 = len(s2)
+
+    if abs(l1 - l2) > 1:
+        return False
+
+    if l1 < l2:
+        shorter, longer = s1, s2
+    else:
+        # might be a misnomer if they're equal length, it doesn't matter
+        # as long as 'longer' isn't shorter than 'shorter'
+        shorter, longer = s2, s1
+
+    equal_length = l1 == l2
+    mismatch_occurred = False
+    i, j = 0, 0
+    while i < len(shorter) and j < len(longer):
+        if shorter[i] != longer[j]:
+            if mismatch_occurred:
+                # second mismatch, second edit occured, fail
+                return False
+            mismatch_occurred = True
+            if equal_length:
+                # "replace" case, move both pointers (j moved later on)
+                i += 1
+        else:
+            # move both pointers (j moved later on) but not for the first mismatch
+            i += 1
+        # always move long string pointer
+        j += 1
+    return True
+
+
 class Test(unittest.TestCase):
     '''Test Cases'''
     data = [
@@ -67,6 +112,8 @@ class Test(unittest.TestCase):
     def test_one_away(self):
         for [test_s1, test_s2, expected] in self.data:
             actual = one_away(test_s1, test_s2)
+            self.assertEqual(actual, expected)
+            actual = one_away__alternative(test_s1, test_s2)
             self.assertEqual(actual, expected)
 
 if __name__ == "__main__":

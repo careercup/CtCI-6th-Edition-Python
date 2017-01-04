2da72f447fea3ec6393d359f91703c7f1fa230d8
Ayesha Omarali <ayesha.omarali@gmail.com>
2017-01-04 05:49:01 -0800

    Update ZeroMatrix.py- enumerate(), 1 func, index()
    Single function, utilizes enumeration to access indices and alter current element in iteration.

--- Chapter01/p08_zero_matrix.py
+++ Chapter01/p08_zero_matrix.py
@@ -1,5 +1,6 @@
 # O(MxN)
 import unittest
+from copy import deepcopy
 
 
 def zero_matrix(matrix):
@@ -33,6 +34,17 @@ def nullify_col(matrix, col):
         matrix[i][col] = 0
 
 
+def zero_matrix_pythonic(matrix):
+    matrix = [["X" if x == 0 else x for x in row] for row in matrix]
+    indices = []
+    for idx, row in enumerate(matrix):
+        if "X" in row:
+            indices = indices + [i for i, j in enumerate(row) if j == "X"]
+            matrix[idx] = [0] * len(matrix[0])
+    matrix = [[0 if row.index(i) in indices else i for i in row] for row in matrix]
+    return matrix
+
+
 class Test(unittest.TestCase):
 
     test_cases = [
@@ -53,11 +65,13 @@ class Test(unittest.TestCase):
             ],
         )
     ]
+    testable_functions = [zero_matrix, zero_matrix_pythonic]
 
     def test_zero_matrix(self):
-        for [test_matrix, expected] in self.test_cases:
-            actual = zero_matrix(test_matrix)
-            self.assertEqual(actual, expected)
+        for f in self.testable_functions:
+            for [test_matrix, expected] in self.test_cases:
+                test_matrix = deepcopy(test_matrix)
+                assert f(test_matrix) == expected
 
 
 if __name__ == "__main__":

659379378ecbfd91262acd80a986d3deecd7af61
Ayesha Omarali <ayesha.omarali@gmail.com>
2017-01-03 23:08:38 -0800

    PalindromePermutation.py - 1 func, uses dic
    Uses dictionary to store keys, utilizes set, all() and built in count()
    I think that this is a solid proposal because if you choose to do an interview in an "englishy" language like Python, it's important to show advanced knowledge of the language and utilize its benefits.

--- Chapter 1/4_Palindrome Permutation/PalindromePermutation.py
+++ Chapter 1/4_Palindrome Permutation/PalindromePermutation.py
@@ -4,31 +4,25 @@ import unittest
 
 def pal_perm(phrase):
     '''function checks if a string is a permutation of a palindrome or not'''
-    table = [0 for _ in range(ord('z') - ord('a') + 1)]
-    countodd = 0
-    for c in phrase:
-        x = char_number(c)
-        if x != -1:
-            table[x] += 1
-            if table[x] % 2:
-                countodd += 1
-            else:
-                countodd -= 1
+    phrase = phrase.replace(" ", "").lower()
+    dic = {}
+    for i in phrase:
+        if i in dic.keys():
+            dic[i] += 1
+        else:
+            dic[i] = 1
+    if len(phrase) % 2 == 0:
+        if all(value % 2 == 0 for value in set(dic.values())):
+            return True
+    else:
+        print "touches odd"
+        vals = dic.values()
+        if vals.count(1) == 1:
+            vals.remove(1)
+            if all(value % 2 == 0 for value in set(vals)):
+                return True
+    return False
 
-    return countodd <= 1
-
-def char_number(c):
-    a = ord('a')
-    z = ord('z')
-    A = ord('A')
-    Z = ord('Z')
-    val = ord(c)
-
-    if a <= val <= z:
-        return val - a
-    elif A <= val <= Z:
-        return val - A
-    return -1
 
 
 class Test(unittest.TestCase):

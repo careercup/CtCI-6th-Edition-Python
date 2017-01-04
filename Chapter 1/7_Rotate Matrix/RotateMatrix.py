0ff044985247d5701bd001d8bd1c3ef54def382f
Ayesha Omarali <ayesha.omarali@gmail.com>
2017-01-04 04:57:02 -0800

    Update RotrateMatrix.py - shorter, zip, range()
    This utilizes zip() in order to simultaneously access the indices increasing and decreasing to properly assign values. 
    This also demonstrates understanding of list comprehensions, and how []*n works, since it cannot be lazily written as [[]*n]*n for assigning different values in this case. 
    Also, I added another test case -- a more simple one so when introducing the problem you can visualize it a little easier.

--- Chapter 1/7_Rotate Matrix/RotateMatrix.py
+++ Chapter 1/7_Rotate Matrix/RotateMatrix.py
@@ -4,30 +4,26 @@ import unittest
 
 def rotate_matrix(matrix):
     '''rotates a matrix 90 degrees clockwise'''
-    n = len(matrix)
-    for layer in range(n // 2):
-        first, last = layer, n - layer - 1
-        for i in range(first, last):
-            # save top
-            top = matrix[layer][i]
-
-            # left -> top
-            matrix[layer][i] = matrix[-i - 1][layer]
-
-            # bottom -> left
-            matrix[-i - 1][layer] = matrix[-layer - 1][-i - 1]
-
-            # right -> bottom
-            matrix[-layer - 1][-i - 1] = matrix[i][- layer - 1]
-
-            # top -> right
-            matrix[i][- layer - 1] = top
-    return matrix
+    x = len(matrix)
+    result = [[0]*x for i in range(x)] #empty list of 0s
+    for i, j in zip(range(x), range(x-1, -1, -1)): #i counts up, j counts down
+        for k in range(x):
+            result[k][i] = matrix[j][k]
+    return result
 
 
 class Test(unittest.TestCase):
     '''Test Cases'''
     data = [
+        ([
+            [1, 2, 3],
+            [4, 5, 6],
+            [7, 8, 9]
+        ], [
+            [7, 4, 1],
+            [8, 5, 2],
+            [9, 6, 3]
+         ]),
         ([
             [1, 2, 3, 4, 5],
             [6, 7, 8, 9, 10],

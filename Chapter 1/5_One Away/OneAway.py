ccda05a21225b9b73e8305fabb6d4449c1f26806
Ayesha Omarali <ayesha.omarali@gmail.com>
2017-01-03 23:51:39 -0800

    OneAway.py - done in 1 func, uses sets(), dic()
    Do it all in one function -- as the hints in the book say. Utilize sets() and dic() for more advanced python techniques. Subtraction of sets show the implementation of finding differences between them!

--- Chapter 1/5_One Away/OneAway.py
+++ Chapter 1/5_One Away/OneAway.py
@@ -2,41 +2,21 @@
 import unittest
 
 
-def one_away(s1, s2):
-    '''Check if a string can converted to another string with a single edit'''
-    if len(s1) == len(s2):
-        return one_edit_replace(s1, s2)
-    elif len(s1) + 1 == len(s2):
-        return one_edit_insert(s1, s2)
-    elif len(s1) - 1 == len(s2):
-        return one_edit_insert(s2, s1)
-    return False
-
-
-def one_edit_replace(s1, s2):
-    edited = False
-    for c1, c2 in zip(s1, s2):
-        if c1 != c2:
-            if edited:
-                return False
-            edited = True
-    return True
-
-
-def one_edit_insert(s1, s2):
-    edited = False
-    i, j = 0, 0
-    while i < len(s1) and j < len(s2):
-        if s1[i] != s2[j]:
-            if edited:
-                return False
-            edited = True
-            j += 1
+def one_away(str1, str2):
+    d1, d2 = {}, {}
+    for i in str1.lower():
+        if i in d1.keys():
+            d1[i] += 1
         else:
-            i += 1
-            j += 1
-    return True
-
+            d1[i] = 1
+    for i in str2.lower():
+        if i in d2.keys():
+            d2[i] += 1
+        else:
+            d2[i] = 1
+    if len(set(d1.items()) - set(d2.items())) in [0, 1] and len(set(d2.items()) - set(d1.items())) in [0, 1]:
+        return True
+    return False
 
 class Test(unittest.TestCase):
     '''Test Cases'''

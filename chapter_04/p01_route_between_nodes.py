b973600a86d7b570e0d334ba57e8f46d3318141a
Ayesha Omarali <ayesha.omarali@gmail.com>
2017-01-06 01:26:05 -0800

    refactor(C04_P01): simpler solution, uses dict, no imports
    Dict/hash table for graph (as suggested by book) and included visualization in comments.
    Also included really simple BFS method so no concern for queue import

--- chapter_04/p01_route_between_nodes.py
+++ chapter_04/p01_route_between_nodes.py
@@ -1,115 +1,71 @@
-import queue
+import unittest
+
+# VISUAL OF TEST GRAPH:
+
+# A -- B
+# |    |
+# C -- D
+# |
+# E -- F -- G -- H
+#      | \
+#      O   I -- J -- K
+#               |
+#               L
+
+# P -- Q
+# |  /
+# R
 
 
-class Graph:
-    def __init__(self):
-        self.max_vertices = 6
-        self.vertices = [0] * self.max_vertices
-        self.count = 0
-
-    def addNode(self, x):
-        if self.count < self.max_vertices:
-            self.vertices[self.count] = x
-            self.count += 1
-        else:
-            print("Graph full")
-
-    def getNodes(self):
-        return self.vertices
-
-
-class Node:
-    def __init__(self, vertex, adjacentLength):
-        self.adjacent = [0] * adjacentLength
-        self.vertex = vertex
-        self.adjacentCount = 0
-        self.visited = False
-
-    def addAdjacent(self, x):
-        if self.adjacentCount < len(self.adjacent):
-            self.adjacent[self.adjacentCount] = x
-            self.adjacentCount += 1
-        else:
-            print("No more adjacent nodes can be added")
-
-    def getAdjacent(self):
-        return self.adjacent
-
-    def getVertex(self):
-        return self.vertex
-
-
-def breadthfirstsearch(g, start, end):
-    if start == end:
-        return True
-    q = queue.Queue(len(g.getNodes()))
-    start.visited = True
-    q.put(start)
-    while not q.empty():
-        r = q.get()
-        if r != None:
-            adjacent = r.getAdjacent()
-            for i in range(len(adjacent)):
-                if adjacent[i].visited == False:
-                    if adjacent[i] == end:
-                        return True
-                    else:
-                        q.put(adjacent[i])
-                    adjacent[i].visited = True
+def is_route(graph, start, end, visited=None):
+    if visited is None:
+        visited = set()
+    for node in graph[start]:
+        if node not in visited:
+            visited.add(node)
+            if node == end or is_route(graph, node, end, visited):
+                return True
     return False
 
 
-def createNewGraph():
-    g = Graph()
-    sizegraph = 6
-    temp = [0] * sizegraph
+class Test(unittest.TestCase):
 
-    temp[0] = Node("a", 3)
-    temp[1] = Node("b", 0)
-    temp[2] = Node("c", 0)
-    temp[3] = Node("d", 1)
-    temp[4] = Node("e", 1)
-    temp[5] = Node("f", 0)
+    graph = {
+        "A": ["B", "C"],
+        "B": ["D"],
+        "C": ["D", "E"],
+        "D": ["B", "C"],
+        "E": ["C", "F"],
+        "F": ["E", "O", "I", "G"],
+        "G": ["F", "H"],
+        "H": ["G"],
+        "I": ["F", "J"],
+        "O": ["F"],
+        "J": ["K", "L", "I"],
+        "K": ["J"],
+        "L": ["J"],
+        "P": ["Q", "R"],
+        "Q": ["P", "R"],
+        "R": ["P", "Q"],
+    }
 
-    temp[0].addAdjacent(temp[1])
-    temp[0].addAdjacent(temp[2])
-    temp[0].addAdjacent(temp[3])
-    temp[3].addAdjacent(temp[4])
-    temp[4].addAdjacent(temp[5])
+    tests = [
+        ("A", "L", True),
+        ("A", "B", True),
+        ("H", "K", True),
+        ("L", "D", True),
+        ("P", "Q", True),
+        ("Q", "P", True),
+        ("Q", "G", False),
+        ("R", "A", False),
+        ("P", "B", False),
+    ]
 
-    for i in range(sizegraph):
-        g.addNode(temp[i])
-    return g
-
-
-def createNewGraphWithLoop():
-    g = Graph()
-    sizegraph = 6
-    temp = [0] * sizegraph
-
-    temp[0] = Node("a", 1)
-    temp[1] = Node("b", 1)
-    temp[2] = Node("c", 1)
-    temp[3] = Node("d", 1)
-    temp[4] = Node("e", 2)
-    temp[5] = Node("f", 0)
-
-    temp[0].addAdjacent(temp[1])
-    temp[1].addAdjacent(temp[2])
-    temp[2].addAdjacent(temp[3])
-    temp[3].addAdjacent(temp[4])
-    temp[4].addAdjacent(temp[1])
-    temp[4].addAdjacent(temp[5])
-
-    for i in range(sizegraph):
-        g.addNode(temp[i])
-    return g
+    def test_is_route(self):
+        for [start, end, expected] in self.tests:
+            actual = is_route(self.graph, start, end)
+            self.assertEqual(actual, expected)
 
 
 if __name__ == "__main__":
-    g = createNewGraphWithLoop()
-    n = g.getNodes()
-    start = n[0]
-    end = n[5]
-    print("Start at:", start.getVertex(), "End at: ", end.getVertex())
-    print(breadthfirstsearch(g, start, end))
+    unittest.main()

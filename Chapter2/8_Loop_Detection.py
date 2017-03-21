e6bc732588601d0a98e5b1bc44d83644b910978d
Mertcan Mermerkaya <mertcan@mermerkaya.net>
2017-03-21 22:42:33 +0000

    Fix loop detection. Closes #24

--- Chapter2/8_Loop_Detection.py
+++ Chapter2/8_Loop_Detection.py
@@ -4,9 +4,11 @@ from LinkedList import LinkedList
 def loop_detection(ll):
     fast = slow = ll.head
 
-    while fast and fast.next and fast is not slow:
+    while fast and fast.next:
         fast = fast.next.next
         slow = slow.next
+        if fast is slow:
+            break
 
     if fast is None or fast.next is None:
         return None

14f5e621f8070087da464ad8d3b681ba22abf979
Jason007 <laujunbupt0203@163.com>
2017-04-21 14:27:27 +0800

    fix(C10_P01): fix bug with equality signs. add test
    when A=[9,10,11,12,13,0,0,0,0], B=[4,5,6,7],the output is [9,4,5,6,7,10,11,12,13] ,but expected [4,5,6,7,9,10,11,12,13]. so i have fixed it and it works well.

--- chapter_10/p01_sorted_merge.py
+++ chapter_10/p01_sorted_merge.py
@@ -4,13 +4,14 @@ def sorted_merge(a, b):
     index_a = len(a) - len(b) - 1
 
     while index_b >= 0:
-        if index_a > 0 and a[index_a] > b[index_b]:
+        if index_a >= 0 and a[index_a] > b[index_b]:
             a[index] = a[index_a]
             index_a -= 1
         else:
             a[index] = b[index_b]
             index_b -= 1
-        index -= 1
+        if index > 0:
+            index -= 1
     return a
 
 
@@ -20,6 +21,13 @@ def fill_array_up_to(maxnum):
 
 def fill_array_with_buffer(length, buffer):
     return [3 * i + 1 for i in range(length)] + ([0] * buffer)
+
+
+def test_sorted_merge():
+    a = [9, 10, 11, 12, 13, 0, 0, 0, 0]
+    b = [4, 5, 6, 7]
+    expected = [4, 5, 6, 7, 9, 10, 11, 12, 13]
+    assert sorted_merge(a, b) == expected
 
 
 def example():

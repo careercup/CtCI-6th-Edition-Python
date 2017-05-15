0e632cb1cf2d55368a41401076ae293ed1c78c8d
WATTBA <nugentc@uoguelph.ca>
2017-05-14 22:17:57 -0400

    unit test added as per repository convention

--- Chapter1/1_Is Unique/IsUnique.py
+++ Chapter1/1_Is Unique/IsUnique.py
@@ -1,5 +1,26 @@
 
 # O(N) time?
+import unittest
 
-if len(set(x)) != len(x):
-    print('not all unique')
+def unique(string):
+	if len(set(x)) != len(x):
+	    return False
+	else:
+		return True
+
+class Test(unittest.TestCase):
+    dataT = [('abcd'), ('s4fad'), ('')]
+    dataF = [('23ds2'), ('hb 627jh=j ()')]
+
+    def test_unique(self):
+        # true check
+        for test_string in self.dataT:
+            actual = unique(test_string)
+            self.assertTrue(actual)
+        # false check
+        for test_string in self.dataF:
+            actual = unique(test_string)
+            self.assertFalse(actual)
+
+if __name__ == "__main__":
+    unittest.main()

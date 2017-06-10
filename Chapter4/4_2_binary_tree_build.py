8fab7957b9e524081b40b18c44589ec8033b2874
WATTBA <nugentc@uoguelph.ca>
2017-06-09 21:15:12 -0400

    missing plus sign and variable cleanup
    I find this list/tuple double method easier to read.

--- Chapter4/4_2_binary_tree_build.py
+++ Chapter4/4_2_binary_tree_build.py
@@ -7,17 +7,19 @@ class tree_build:
 	def __init__(self, l_list):
 		self.in_list = l_list
 	def binary_tree(self, list_of_nodes):
-		end = len(list_of_nodes)
-		root = int(((end) / 2))
+		root = int((len(list_of_nodes) // 2))
 		middle = list_of_nodes[root]
 		left = list_of_nodes[:(root-1)]
-		right = list_of_nodes[root:]
+		right = list_of_nodes[root+1:]
 		if len(right) > 1:
 			right = self.binary_tree(right)
 		if len(left) > 1:
 			left = self.binary_tree(left)
 		output = (left, ':L', middle, 'R:', right)
-
+		if (len(right) == 0)  or (str(right) == middle):
+			output = output[:-2]
+		if len(left) == 0 or (left == middle):
+			output = output[2:]
 		return output
 	def tree(self):
 		list_of_nodes = self.in_list

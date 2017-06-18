5675ef18278f975fa587c7219b0b27e2373c6519
WATTBA <nugentc@uoguelph.ca>
2017-06-17 21:05:44 -0400

    the debug wars begin

--- Chapter5/5_1_insert_bits.py
+++ Chapter5/5_1_insert_bits.py
@@ -1,13 +1,9 @@
 
-"""question says to inser one number into another,
-	it is not clear on if it wants addition, I'm
-	doing it with addition, if just a straight
-	slice in needed, will change if wrong when 
-	I read the answers section """
+
 
 def insert_bits(major, minor, start, end):
 	temp = str(major)[-(end+1):-(start)]
-	new = bin(int(minor,2) + int(temp,2))
+	new = bin(int(minor,2))
 	new_major = major[:-(end+1)] + new[2:] + major[-start:]
 	return new_major
 
--- Chapter5/5_3_flip_bit.py
+++ Chapter5/5_3_flip_bit.py
@@ -11,24 +11,32 @@
 def flip_bit(int_or_bi_string):
 	if type(int_or_bi_string) == int:
 		int_or_bi_string = bin(int_or_bi_string)[2:]
-	max_count = 0
-	current_len = 0
-	current_0s = 0
-	for i in int_or_bi_string:
-		if i == '1':
-			current_len += 1
-		elif (i=='0') and (current_0s == 0):
-			current_0s += 1
-			current_len += 1
-		elif (i=='0') and (current_0s == 1):
-			current_0s = 0
-			if current_len > max_count:
-				max_count = current_len
-			continue
-	if current_len > max_count:
-		max_count = current_len
-	return max_count
+	list_of_types = []
+	counter = 1
+	current = int_or_bi_string[0]
+	for z, i in enumerate(int_or_bi_string[1:]):
+		if i == current:
+			counter += 1
+			if z == (len(int_or_bi_string[1:])-1):
+				list_of_types.append((current, counter))
+		else:
+			list_of_types.append((current, counter))
+			current = i 
+			counter = 1
+			if z == (len(int_or_bi_string[:1])-1):
+				list_of_types.append((current, counter))
+	len_max = 0
+	for x in range(0,len(list_of_types)):
+		if (list_of_types[x][0] == '0' ) and (list_of_types[x][1] == 1):
+			if(x == (len(list_of_types)-1)):
+				current = (list_of_types[x-1][1])+(list_of_types[x][1])
+			else:
+				current = (list_of_types[x-1][1])+(list_of_types[x][1])+(list_of_types[x+1][1])
+			if current > len_max:
+				len_max = current
+	return len_max
 
-flip_bit(1775)
+flip_bit('0001110111')
 
-in_bin='1010011010'
+
+

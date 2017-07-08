140329939526c029e46176e36f7394cbb5270678
Nikolay Derkach <nderk@me.com>
2017-07-07 22:46:03 -0500

    Update StringCompression.py
    Start counter with 1

--- Chapter1/6_String Compression/StringCompression.py
+++ Chapter1/6_String Compression/StringCompression.py
@@ -4,12 +4,12 @@ import unittest
 
 def string_compression(string):
     compressed = []
-    counter = 0
+    counter = 1
 
-    for i in range(len(string)):
-        if i != 0 and string[i] != string[i - 1]:
+    for i in range(1, len(string)):
+        if string[i] != string[i - 1]:
             compressed.append(string[i - 1] + str(counter))
-            counter = 0
+            counter = 1
         counter += 1
 
     # add last repeated character

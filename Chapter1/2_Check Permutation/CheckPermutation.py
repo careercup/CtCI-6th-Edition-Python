69659e130d3c3ee8195aaa4a302fdce0e3393182
Karan Chawla <karangc2@illinois.edu>
2017-09-19 18:37:16 -0500

    More pythonic version

--- Chapter1/2_Check Permutation/CheckPermutation.py
+++ Chapter1/2_Check Permutation/CheckPermutation.py
@@ -6,14 +6,7 @@ from collections import Counter
 def check_permutation(str1, str2):
     if len(str1) != len(str2):
         return False
-    counter = Counter()
-    for c in str1:
-        counter[c] += 1
-    for c in str2:
-        if counter[c] == 0:
-            return False
-        counter[c] -= 1
-    return True
+    return not (Counter(str1) - Counter(str2))
 
 
 class Test(unittest.TestCase):

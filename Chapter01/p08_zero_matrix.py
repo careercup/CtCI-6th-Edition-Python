d49d58f17a44c3a0382e4940c27adb699cdd0b11
Dmitry Ishkov <dimanrussian@gmail.com>
2017-11-19 14:45:08 +0530

    optimise for the case when all elements are zero
    * don't nullify rows/columns twice for the same row/column (via using sets)
    * only touch each element once when nullifying

--- Chapter01/p08_zero_matrix.py
+++ Chapter01/p08_zero_matrix.py
@@ -6,32 +6,21 @@ from copy import deepcopy
 def zero_matrix(matrix):
     m = len(matrix)
     n = len(matrix[0])
-    rows = []
-    cols = []
+    rows = set()
+    cols = set()
 
     for x in range(m):
         for y in range(n):
             if matrix[x][y] == 0:
-                rows.append(x)
-                cols.append(y)
+                rows.add(x)
+                cols.add(y)
 
-    for row in rows:
-        nullify_row(matrix, row)
-
-    for col in cols:
-        nullify_col(matrix, col)
+    for x in range(m):
+        for y in range(n):
+            if (x in rows) or (y in cols):
+                matrix[x][y] = 0
 
     return matrix
-
-
-def nullify_row(matrix, row):
-    for i in range(len(matrix[0])):
-        matrix[row][i] = 0
-
-
-def nullify_col(matrix, col):
-    for i in range(len(matrix)):
-        matrix[i][col] = 0
 
 
 def zero_matrix_pythonic(matrix):

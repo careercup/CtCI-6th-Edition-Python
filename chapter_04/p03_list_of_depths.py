0df270b97de7338139a04372616e5f7df08bb39c
Kyle Lim <kylelim8998@gmail.com>
2018-04-01 21:44:58 -0400

    feature(C04_P03): added alternative implementation

--- chapter_04/p03_list_of_depths.py
+++ chapter_04/p03_list_of_depths.py
@@ -1,31 +1,13 @@
 from collections import deque
 
-
-class LinkedNode:
-    def __init__(self, name):
-        self.name = name
-        self.next = None
-
-    def get_last(self):
-        node = self
-        while node.next:
-            node = node.next
-        return node
-
-    def __repr__(self):
-        rv = []
-        node = self
-        while node:
-            rv.append(node.name)
-            node = node.next
-        return str(rv)
+from chapter_02.linked_list import LinkedList
 
 
 class BinaryNode:
-    def __init__(self, name):
+    def __init__(self, name, left=None, right=None):
         self.name = name
-        self.left = None
-        self.right = None
+        self.left = left
+        self.right = right
 
 
 def create_node_list_by_depth(tree_root):
@@ -38,11 +20,10 @@ def create_node_list_by_depth(tree_root):
         node, level = q.popleft()
         if level not in levels:
             # First node in the level
-            levels[level] = LinkedNode(node.name)
-        else:
-            # Nodes already exist
-            last = levels[level].get_last()
-            last.next = LinkedNode(node.name)
+            levels[level] = LinkedList()
+        # Nodes already exist
+        levels[level].add(node)
+
         # Push onto queue
         if node.left:
             q.append((node.left, level + 1))
@@ -51,17 +32,45 @@ def create_node_list_by_depth(tree_root):
     return levels
 
 
-def test_create_node_list_by_depth():
-    root = BinaryNode(0)
-    root.left = BinaryNode(1)
-    root.right = BinaryNode(2)
-    root.left.left = BinaryNode(3)
-    root.left.right = BinaryNode(4)
-    root.right.left = BinaryNode(5)
-    root.right.right = BinaryNode(6)
+def create_node_list_by_depth_b(tree):
+    if not tree:
+        return []
 
-    levels = create_node_list_by_depth(root)
-    assert repr(levels) == repr({0: [0], 1: [1, 2], 2: [3, 4, 5, 6]})
+    curr = tree
+    result = [LinkedList([curr])]
+    level = 0
+
+    while result[level]:
+        result.append(LinkedList())
+        for linked_list_node in result[level]:
+            n = linked_list_node.value
+            if n.left:
+                result[level + 1].add(n.left)
+            if n.right:
+                result[level + 1].add(n.right)
+        level += 1
+    return result
+
+
+testable_functions = [create_node_list_by_depth, create_node_list_by_depth_b]
+
+
+def test_create_node_list_by_depth():
+    for f in testable_functions:
+        node_h = BinaryNode("H")
+        node_g = BinaryNode("G")
+        node_f = BinaryNode("F")
+        node_e = BinaryNode("E", node_g)
+        node_d = BinaryNode("D", node_h)
+        node_c = BinaryNode("C", None, node_f)
+        node_b = BinaryNode("B", node_d, node_e)
+        node_a = BinaryNode("A", node_b, node_c)
+        lists = f(node_a)
+
+        assert lists[0].values() == LinkedList([node_a]).values()
+        assert lists[1].values() == LinkedList([node_b, node_c]).values()
+        assert lists[2].values() == LinkedList([node_d, node_e, node_f]).values()
+        assert lists[3].values() == LinkedList([node_h, node_g]).values()
 
 
 if __name__ == "__main__":

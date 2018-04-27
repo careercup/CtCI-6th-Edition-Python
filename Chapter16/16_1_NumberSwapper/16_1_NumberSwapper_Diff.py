459c619c8345ea5918dfdd6a31ed733be5cc0f68
hchiam <hchiam@gmail.com>
2018-04-26 20:54:20 -0400

    16.1 Number Swapper

---
+++ Chapter16/16_1_NumberSwapper/16_1_NumberSwapper_Diff.py
@@ -0,0 +1,31 @@
+import unittest
+
+def swap_numbers(a, b):
+    a = b-a
+    b = b-a # = b-(b-a) = b-b+a = a
+    a = a+b # = (b-a)+a = b-a+a = b
+    return a, b
+
+class Test(unittest.TestCase):
+    
+    def test_small_big(self):
+        pairs = [
+            [1,2],
+            [10,3],
+            [4,4],
+            [1,0],
+            [7,-4],
+            [-7,-4]
+        ]
+        for pair in pairs:
+            a_original = pair[0]
+            b_original = pair[1]
+            a, b = swap_numbers(a_original, b_original)
+            self.assertTrue(a == b_original)
+            self.assertTrue(b == a_original)
+
+def main():
+    unittest.main()
+
+if __name__ == '__main__':
+    main()

0c4f18c8085d12a9a55a340370be30b511183d85
hchiam <hchiam@gmail.com>
2018-04-26 21:18:43 -0400

    make lowercase, remove punctuation

--- Chapter16/16_2_WordFrequencies/16_2_SingleQuery.py
+++ Chapter16/16_2_WordFrequencies/16_2_SingleQuery.py
@@ -1,18 +1,22 @@
+import string
+
 def get_frequency(book, word):
     if book == None or word == None:
         return 0
     word = word.lower()
     count = 0
     for book_word in book.split():
-        if book_word.lower() == word:
+        # make lowercase, remove punctuation
+        book_word = book_word.lower().translate(None, string.punctuation)
+        if book_word == word:
             count += 1
     return count
 
 book = 'Once upon a time there was this book. \
         This is a sentence. This is a much longer sentence. \
-        This is a terribly short example. But you get the idea. \
+        This book is terribly short. But you get the idea. \
         You should see the word this 6 times in this example text.'
 
-word = 'this'
+word = 'book'
 
 print('The word "' + word + '" appears ' + str(get_frequency(book, word)) + ' times.')

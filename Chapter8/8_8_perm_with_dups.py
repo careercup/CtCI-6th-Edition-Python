f3e180db0a660bf35134a7435697100288cccf4a
CNuge <nugentc@uoguelph.ca>
2018-08-21 20:34:28 -0400

    permutations with duplicates

---
+++ Chapter8/8_8_perm_with_dups.py
@@ -0,0 +1,38 @@
+
+
+def Permutations(s):
+	""" take a string as an input and returns a list of all 
+		permutations of that string """
+
+	perms = [s]
+
+	if len(s) == 1:
+		return perms
+
+	for pos, i in enumerate(s):
+
+		rest_of_string = s[:pos] + s[pos+1:]
+
+		sub_perms = Permutations(rest_of_string)
+
+		for sub in sub_perms:
+			if sub not in perms:
+				perms.append(i+sub)
+
+	return perms
+
+
+
+
+
+if __name__ == '__main__':
+
+
+	s = 'test16'
+
+	test_out = Permutations(s)
+
+	for x in test_out:
+		print(x)
+
+	print(len(test_out))

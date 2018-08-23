97008d5e09b0bc40ec34b05b78f36032dd289904
CNuge <nugentc@uoguelph.ca>
2018-08-23 15:06:41 -0400

    formatting

--- revisiting_ctci/1_3_URLify.go
+++ revisiting_ctci/1_3_URLify.go
@@ -1,22 +1,20 @@
 package main
 
-
-import(
+import (
 	"fmt"
 	"strings"
 )
 
 // take the string and replace all spaces with %20
 func URL(s string) string {
-	return strings.Replace(s," ", "%20", -1 )
+	return strings.Replace(s, " ", "%20", -1)
 }
 
-
-func main(){
+func main() {
 
 	t1 := "this is the string"
 
-	fmt.Printf("words: %v\n",  t1)
-	fmt.Printf("url: %v\n", URL(t1) )
+	fmt.Printf("words: %v\n", t1)
+	fmt.Printf("url: %v\n", URL(t1))
 
 }

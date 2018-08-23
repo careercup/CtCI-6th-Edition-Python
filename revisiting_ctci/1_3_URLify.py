7e31c2e49b857054479c18fb230fc250273ae91f
CNuge <nugentc@uoguelph.ca>
2018-08-23 15:22:46 -0400

    cleanup

--- revisiting_ctci/1_3_URLify.py
+++ revisiting_ctci/1_3_URLify.py
@@ -1,16 +1,12 @@
 import unittest
 
-
-
 def URL(s):
 	""" take an input string and replace spaces with %20"""
 	return s.replace(" ", "%20")
 
-
 class TestURL(unittest.TestCase):
 	def test_URLfunc(self):
 		self.assertEqual(URL("this is the string"), "this%20is%20the%20string")
-
 
 if __name__ == '__main__':
 	unittest.main()

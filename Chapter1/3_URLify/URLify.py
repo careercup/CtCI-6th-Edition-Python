ab96776753cad15ac06b38274cb75fe17ed9e483
Thomas <thomasjk10@gmail.com>
2018-10-09 22:27:50 +0530

    Update URLify.py
    Couldn't this be used ? Or we aren't allowed to use the .replace() ?

--- Chapter1/3_URLify/URLify.py
+++ Chapter1/3_URLify/URLify.py
@@ -1,36 +1,11 @@
-# O(N)
-import unittest
+#-----------------------------------------------------------------------
+# Strip all spaces at the end of string and then replace spaces with %20
+#-----------------------------------------------------------------------
+def urlify(s):
+    new_s = s.rstrip()
+    url = new_s.replace(" ", "%20")
+    return url
 
-
-def urlify(string, length):
-    '''function replaces single spaces with %20 and removes trailing spaces'''
-    new_index = len(string)
-
-    for i in reversed(range(length)):
-        if string[i] == ' ':
-            # Replace spaces
-            string[new_index - 3:new_index] = '%20'
-            new_index -= 3
-        else:
-            # Move characters
-            string[new_index - 1] = string[i]
-            new_index -= 1
-
-    return string
-
-
-class Test(unittest.TestCase):
-    '''Test Cases'''
-    # Using lists because Python strings are immutable
-    data = [
-        (list('much ado about nothing      '), 22,
-         list('much%20ado%20about%20nothing')),
-        (list('Mr John Smith    '), 13, list('Mr%20John%20Smith'))]
-
-    def test_urlify(self):
-        for [test_string, length, expected] in self.data:
-            actual = urlify(test_string, length)
-            self.assertEqual(actual, expected)
-
-if __name__ == "__main__":
-    unittest.main()
+inp = "I am$ be is fine,9"
+test = inp.split(',')[0]
+make_url = urlify(test)

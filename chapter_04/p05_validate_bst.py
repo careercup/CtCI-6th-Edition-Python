1822ea167d9165d628bb1351cbba32e8376b888b
Damien Lancry <31038265+DamienLancry@users.noreply.github.com>
2018-11-25 12:12:49 +0100

    feature(C04_P05): update to use real trees

--- chapter_04/p05_validate_bst.py
+++ chapter_04/p05_validate_bst.py
@@ -1,28 +1,39 @@
-def is_binary_search_tree(node, edges):
-    children = edges[node]
-    property = True
-    if not children:
-        return property
-    left = children[0]
-    right = children[1]
-    if left:
-        property &= left < node
-        property &= is_binary_search_tree(left, edges)
-    if right:
-        property &= node < right
-        property &= is_binary_search_tree(right, edges)
-    return property
+from chapter_04.binary_search_tree import BinarySearchTree
+from chapter_04.binary_tree import BinaryTree
 
 
-if __name__ == "__main__":
-    """
-         4                          4
-     2       6    = True;       2       6    = False
-    1 3     5 7                3 1     7 5
-    """
-    print(
-        is_binary_search_tree(4, [None, None, [1, 3], None, [2, 6], None, [5, 7], None])
+def is_binary_search_tree(tree):
+    return _is_bst(tree.root)
+
+
+def _is_bst(node, min_val=None, max_val=None):
+    if not node:
+        return True
+    if (min_val and node.key < min_val) or (max_val and node.key >= max_val):
+        return False
+    return _is_bst(node.left, min_val, node.key) and _is_bst(
+        node.right, node.key, max_val
     )
-    print(
-        is_binary_search_tree(4, [None, None, [3, 1], None, [2, 6], None, [7, 5], None])
-    )
+
+
+def test_is_binary_search_tree():
+    bst = BinarySearchTree()
+    bst.insert(20)
+    bst.insert(9)
+    bst.insert(25)
+    bst.insert(5)
+    bst.insert(12)
+    bst.insert(11)
+    bst.insert(14)
+
+    t = BinaryTree()
+    n1 = t.insert(5, None)
+    n2 = t.insert(4, n1)
+    n3 = t.insert(6, n1)
+    n4 = t.insert(3, n2)
+    t.insert(6, n2)
+    t.insert(5, n3)
+    t.insert(2, n4)
+
+    assert not is_binary_search_tree(t)
+    assert is_binary_search_tree(bst)

fa8acf46e33331e2fe278019b35f2ead44fbc629
Damien Lancry <31038265+DamienLancry@users.noreply.github.com>
2018-12-12 17:18:23 +0100

    feature(C04_P09): add solution

---
+++ chapter_04/p09_bst_sequences.py
@@ -0,0 +1,64 @@
+from chapter_04.binary_search_tree import BinarySearchTree
+
+
+def find_bst_sequences(bst):
+    if not bst.root:
+        return []
+    return helper(bst.root)
+
+
+def helper(node):
+    if not node:
+        return [[]]
+    else:
+        right_sequences = helper(node.right)
+        left_sequences = helper(node.left)
+        sequences = []
+        for right in right_sequences:
+            for left in left_sequences:
+                sequences = weave(left, right, [node.key], sequences)
+        return sequences
+
+
+def weave(first, second, prefix, results):
+    if len(first) == 0 or len(second) == 0:
+        result = prefix.copy()
+        result.extend(first)
+        result.extend(second)
+        results.append(result)
+        return results
+
+    head = first[0]
+    prefix.append(head)
+    results = weave(first[1:], second, prefix, results)
+    prefix.pop()
+    head = second[0]
+    prefix.append(head)
+    results = weave(first, second[1:], prefix, results)
+    prefix.pop()
+    return results
+
+
+def test_find_bst_sequences():
+    bst = BinarySearchTree()
+    bst.insert(2)
+    bst.insert(1)
+    bst.insert(3)
+    sequences = find_bst_sequences(bst)
+    assert [2, 1, 3] in sequences
+    assert [2, 3, 1] in sequences
+    assert len(sequences) == 2
+
+
+if __name__ == "__main__":
+    bst = BinarySearchTree()
+    bst.insert(20)
+    bst.insert(9)
+    bst.insert(25)
+    bst.insert(5)
+    bst.insert(12)
+    # bst.insert(11);
+    # bst.insert(14);
+
+    sequences = find_bst_sequences(bst)
+    print(sequences)

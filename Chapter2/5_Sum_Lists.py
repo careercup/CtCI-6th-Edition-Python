f943b6f26cc6848349038fe4ab1ab518441b09b5
kasturinaveen <43032059+kasturinaveen@users.noreply.github.com>
2019-02-11 22:20:26 +0530

    Modified for optimization

--- Chapter2/5_Sum_Lists.py
+++ Chapter2/5_Sum_Lists.py
@@ -2,44 +2,20 @@ from LinkedList import LinkedList
 
 
 def sum_lists(ll_a, ll_b):
+    a_val =0
+    b_val = 0
+    node_num = 0
     n1, n2 = ll_a.head, ll_b.head
-    ll = LinkedList()
-    carry = 0
-    while n1 or n2:
-        result = carry
-        if n1:
-            result += n1.value
-            n1 = n1.next
-        if n2:
-            result += n2.value
-            n2 = n2.next
-
-        ll.add(result % 10)
-        carry = result // 10
-
-    if carry:
-        ll.add(carry)
-
-    return ll
-
-
-def sum_lists_followup(ll_a, ll_b):
-    # Pad the shorter list with zeros
-    if len(ll_a) < len(ll_b):
-        for i in range(len(ll_b) - len(ll_a)):
-            ll_a.add_to_beginning(0)
-    else:
-        for i in range(len(ll_a) - len(ll_b)):
-            ll_b.add_to_beginning(0)
-
-    # Find sum
-    n1, n2 = ll_a.head, ll_b.head
-    result = 0
-    while n1 and n2:
-        result = (result * 10) + n1.value + n2.value
+    while n1:
+        a_val = a_val + n1.value*(10**node_num)
         n1 = n1.next
+        node_num+=node_num
+    node_num = 0
+    while n2:
+        b_val = b_val+ n2.value*(10**node_num)
         n2 = n2.next
-
+        node_num+=node_num
+    result = a_val+b_val
     # Create new linked list
     ll = LinkedList()
     ll.add_multiple([int(i) for i in str(result)])
@@ -54,4 +30,3 @@ ll_b.generate(3, 0, 9)
 print(ll_a)
 print(ll_b)
 print(sum_lists(ll_a, ll_b))
-print(sum_lists_followup(ll_a, ll_b))

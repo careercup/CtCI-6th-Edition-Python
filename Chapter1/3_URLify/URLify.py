44576a51f93bfdb30f30d1114f72c522d5da355d
Brayoni Ian <brayoni.ian@gmail.com>
2019-03-13 09:50:51 +0300

    refactor Problem1.3_URLify to take a str as an argument

--- Chapter1/3_URLify/URLify.py
+++ Chapter1/3_URLify/URLify.py
@@ -3,34 +3,36 @@ import unittest
 
 
 def urlify(string, length):
-    '''function replaces single spaces with %20 and removes trailing spaces'''
-    new_index = len(string)
+    """function replaces single spaces with %20 and removes trailing spaces"""
+    index = len(string)
+    str_list = list(string)
 
     for i in reversed(range(length)):
-        if string[i] == ' ':
+        if str_list[i] == " ":
             # Replace spaces
-            string[new_index - 3:new_index] = '%20'
-            new_index -= 3
+            str_list[index - 3:index] = "%20"
+            index -= 3
         else:
             # Move characters
-            string[new_index - 1] = string[i]
-            new_index -= 1
+            str_list[index - 1] = str_list[i]
+            index -= 1
 
-    return string
+    return "".join(str_list)
 
 
 class Test(unittest.TestCase):
-    '''Test Cases'''
-    # Using lists because Python strings are immutable
+    """Test Cases"""
+
     data = [
-        (list('much ado about nothing      '), 22,
-         list('much%20ado%20about%20nothing')),
-        (list('Mr John Smith    '), 13, list('Mr%20John%20Smith'))]
+        ("much ado about nothing      ", 22, "much%20ado%20about%20nothing"),
+        ("Mr John Smith    ", 13, "Mr%20John%20Smith"),
+    ]
 
     def test_urlify(self):
         for [test_string, length, expected] in self.data:
             actual = urlify(test_string, length)
             self.assertEqual(actual, expected)
+
 
 if __name__ == "__main__":
     unittest.main()

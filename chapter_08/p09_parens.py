be98573e242c67f528ab0fd2c5d57b7922d12eb4
Jonasz Karwacki <jonaszakr@gmail.com>
2019-05-01 13:06:09 -0700

    feature(C08_P09): add non-brute-force solution

--- chapter_08/p09_parens.py
+++ chapter_08/p09_parens.py
@@ -1,3 +1,6 @@
+import unittest
+
+
 def next_permutation(arr):
     i = len(arr) - 1
     while i > 0 and arr[i - 1] >= arr[i]:
@@ -25,7 +28,7 @@ def is_matched_parentheses(ray):
     return True
 
 
-def generate_parentheses_permutations(number_of_pairs):
+def generate_parentheses_permutations_brute_force(number_of_pairs):
     starting_list = (["("] * number_of_pairs) + [")"] * number_of_pairs
     possibilities = ["".join(starting_list)]
     while next_permutation(starting_list):
@@ -34,9 +37,53 @@ def generate_parentheses_permutations(number_of_pairs):
     return possibilities
 
 
+def generate_parentheses_permutations(n):
+    def helper(
+        open_parentheses_remaining, closed_parentheses_remaining, current_string
+    ):
+        if len(current_string) == n * 2:
+            result.append(current_string)
+        if open_parentheses_remaining > 0:
+            helper(
+                open_parentheses_remaining - 1,
+                closed_parentheses_remaining,
+                current_string + "(",
+            )
+        if closed_parentheses_remaining > open_parentheses_remaining:
+            helper(
+                open_parentheses_remaining,
+                closed_parentheses_remaining - 1,
+                current_string + ")",
+            )
+
+    result = []
+    helper(n, n, "")
+    return result
+
+
+testable_functions = [
+    generate_parentheses_permutations_brute_force,
+    generate_parentheses_permutations,
+]
+
+test_cases = [
+    (0, [""]),
+    (1, ["()"]),
+    (2, sorted(["()()", "(())"])),
+    (3, sorted(["((()))", "(()())", "(())()", "()(())", "()()()"])),
+]
+
+
+class TestSuite(unittest.TestCase):
+    def test_generate_parentheses_permutations(self):
+        for f in testable_functions:
+            for num, expected in test_cases:
+                self.assertEqual(sorted(f(num)), expected)
+
+
 def example():
     print(generate_parentheses_permutations(2))
-    print(generate_parentheses_permutations(3))
+    print(generate_parentheses_permutations_brute_force(3))
 
 
 if __name__ == "__main__":

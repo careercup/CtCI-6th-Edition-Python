e93c68850382766ac5643f37d0a2b47212ab315e
Zane Clark <zanebclark@gmail.com>
2019-07-30 22:09:45 -0500

    A different approach

--- Chapter1/3_URLify/URLify.py
+++ Chapter1/3_URLify/URLify.py
@@ -2,21 +2,8 @@
 import unittest
 
 
-def urlify(string, length):
-    '''function replaces single spaces with %20 and removes trailing spaces'''
-    new_index = len(string)
-
-    for i in reversed(range(length)):
-        if string[i] == ' ':
-            # Replace spaces
-            string[new_index - 3:new_index] = '%20'
-            new_index -= 3
-        else:
-            # Move characters
-            string[new_index - 1] = string[i]
-            new_index -= 1
-
-    return string
+def urlify(string, str_len):
+    return "%20".join(string[:str_len].split(' '))
 
 
 class Test(unittest.TestCase):

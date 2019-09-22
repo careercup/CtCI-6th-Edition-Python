7cef3df081a8f6cd37cafb645e8a79cbc947cf9d
Kyle <kylepw@gmail.com>
2019-09-22 22:49:13 +0900

    feature(C03_P04): Add 34 solution

--- chapter_03/p04_queue_via_stacks.py
+++ chapter_03/p04_queue_via_stacks.py
@@ -6,70 +6,97 @@ from chapter_03.stack import Stack
 
 class MyQueue:
     def __init__(self):
-        self.a = Stack()
-        self.b = Stack()
+        self.new_stack = Stack()
+        self.old_stack = Stack()
 
-    def enqueue(self, item):
-        while not self.a.is_empty():
-            self.b.push(self.a.pop())
-        self.b.push(item)
+    def _shift_stacks(self):
+        if self.old_stack.is_empty():
+            while not self.new_stack.is_empty():
+                self.old_stack.push(self.new_stack.pop())
 
-        while not self.b.is_empty():
-            self.a.push(self.b.pop())
+    def add(self, value):
+        return self.new_stack.push(value)
 
-    def dequeue(self):
-        if not self.a.is_empty():
-            return self.a.pop()
-        else:
-            if not self.b.is_empty():
-                return self.b.pop()
-            return None
+    def peek(self):
+        if self.is_empty():
+            return False
+        self._shift_stacks()
+        return self.old_stack.peek()
+
+    def remove(self):
+        if self.is_empty():
+            return False
+        self._shift_stacks()
+        return self.old_stack.pop()
 
     def is_empty(self):
         return len(self) == 0
 
     def __len__(self):
-        return len(self.a) + len(self.b)
+        return len(self.new_stack) + len(self.old_stack)
 
 
 class Tests(unittest.TestCase):
+    def test_peek(self):
+        q = MyQueue()
+        q.add(4)
+        q.add(6)
+        self.assertEqual(q.peek(), 4)
+
+    def test_add(self):
+        q = MyQueue()
+        q.add(4)
+        q.add(6)
+        self.assertEqual(q.peek(), 4)
+        q.add(101)
+        self.assertNotEqual(q.peek(), 101)
+
+    def test_remove(self):
+        q = MyQueue()
+        q.add(4)
+        q.add(6)
+        self.assertEqual(q.remove(), 4)
+        self.assertEqual(q.remove(), 6)
+
+        self.assertFalse(q.remove())
+
     def test_enqueue_one(self):
         queue = MyQueue()
         assert queue.is_empty()
-        queue.enqueue(1)
+        queue.add(1)
         assert not queue.is_empty()
         self.assertEqual(len(queue), 1)
 
     def test_enqueue_two(self):
         queue = MyQueue()
-        queue.enqueue(1)
-        queue.enqueue(2)
+        queue.add(1)
+        queue.add(2)
         self.assertEqual(len(queue), 2)
 
     def test_enqueue_three(self):
         queue = MyQueue()
-        queue.enqueue(1)
-        queue.enqueue(2)
-        queue.enqueue(3)
+        queue.add(1)
+        queue.add(2)
+        queue.add(3)
         self.assertEqual(len(queue), 3)
 
     def test_dequeue_one(self):
         queue = MyQueue()
-        queue.enqueue(1)
-        self.assertEqual(queue.dequeue(), 1)
+        queue.add(1)
+        self.assertEqual(queue.remove(), 1)
 
     def test_dequeue_two(self):
         queue = MyQueue()
-        queue.enqueue(1)
-        queue.enqueue(2)
-        self.assertEqual(queue.dequeue(), 1)
-        self.assertEqual(queue.dequeue(), 2)
+        queue.add(1)
+        queue.add(2)
+        self.assertEqual(queue.remove(), 1)
+        self.assertEqual(queue.remove(), 2)
 
     def test_dequeue_three(self):
         queue = MyQueue()
-        queue.enqueue(1)
-        queue.enqueue(2)
-        queue.enqueue(3)
-        self.assertEqual(queue.dequeue(), 1)
-        self.assertEqual(queue.dequeue(), 2)
-        self.assertEqual(queue.dequeue(), 3)
+        queue.add(1)
+        queue.add(2)
+        queue.add(3)
+        self.assertEqual(queue.remove(), 1)
+        self.assertEqual(queue.remove(), 2)
+        self.assertEqual(queue.remove(), 3)

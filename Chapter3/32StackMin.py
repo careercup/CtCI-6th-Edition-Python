5bb5599abbab4d2d5b0dbb3414fb182e7a94f36a
Rakshit Parashar <34675136+rishu2403@users.noreply.github.com>
2020-07-28 14:14:22 -0700

    Update 32StackMin.py

--- Chapter3/32StackMin.py
+++ Chapter3/32StackMin.py
@@ -64,3 +64,52 @@ def StackMin():
     print newstack.Min(0)
 
 StackMin()
+
+''' Method 2: another stack to keep track of min element 
+
+
+
+class StackWithMax:
+    def __init__(self):
+
+        self.mainStack = []
+
+      
+        self.trackStack = []
+
+    def push(self, x):
+        self.mainStack.append(x)
+        if (len(self.mainStack) == 1):
+            self.trackStack.append(x)
+            return
+
+       
+        if (x < self.trackStack[-1]):
+            self.trackStack.append(x)
+        else:
+            self.trackStack.append(self.trackStack[-1])
+
+    def getMin(self):
+        return self.trackStack[-1]
+
+    def pop(self):
+        self.mainStack.pop()
+        self.trackStack.pop()
+
+
+# Driver Code
+if __name__ == '__main__':
+
+    s = StackWithMax()
+    s.push(20)
+    s.push(23)
+    s.push(60)
+    s.push(700)
+    print(s.getMin())
+
+O(1) time
+O(n) space
+'''
+
+
+

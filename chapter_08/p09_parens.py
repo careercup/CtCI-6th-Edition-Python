0b81d4ca81976e185f8021c211c1861851197fde
Zhihong (George) Li <33303558+georgelzh@users.noreply.github.com>
2020-10-09 17:32:10 -0400

    feature(C08_P09): add non-brute-force solution

--- chapter_08/p09_parens.py
+++ chapter_08/p09_parens.py
@@ -37,7 +37,7 @@ def generate_parentheses_permutations_brute_force(number_of_pairs):
     return possibilities
 
 
-def generate_parentheses_permutations(n):
+def generate_parentheses_permutations_recursive_1(n):
     def helper(
         open_parentheses_remaining, closed_parentheses_remaining, current_string
     ):
@@ -61,9 +61,32 @@ def generate_parentheses_permutations(n):
     return result
 
 
+def add_paren(arr, left_rem, right_rem, string_arr, idx):
+    if left_rem < 0 or right_rem < left_rem:  # invalid
+        return
+    if left_rem == 0 and right_rem == 0:  # out of left and right parentheses
+        elem = "".join(string_arr)
+        arr.append(elem)
+
+    else:
+        string_arr[idx] = "("  # add left and recurse
+        add_paren(arr, left_rem - 1, right_rem, string_arr, idx + 1)
+
+        string_arr[idx] = ")"  # add right and recurse
+        add_paren(arr, left_rem, right_rem - 1, string_arr, idx + 1)
+
+
+def generate_parentheses_permutations_recursive_2(n):
+    results = []
+    string_arr = ["*"] * n * 2
+    add_paren(results, n, n, string_arr, 0)
+    return results
+
+
 testable_functions = [
     generate_parentheses_permutations_brute_force,
-    generate_parentheses_permutations,
+    generate_parentheses_permutations_recursive_1,
+    generate_parentheses_permutations_recursive_2,
 ]
 
 test_cases = [
@@ -79,11 +102,13 @@ class TestSuite(unittest.TestCase):
         for f in testable_functions:
             for num, expected in test_cases:
                 self.assertEqual(sorted(f(num)), expected)
+                print(expected)
 
 
 def example():
-    print(generate_parentheses_permutations(2))
+    print(generate_parentheses_permutations_recursive_1(2))
     print(generate_parentheses_permutations_brute_force(3))
+    print(generate_parentheses_permutations_recursive_2(3))
 
 
 if __name__ == "__main__":

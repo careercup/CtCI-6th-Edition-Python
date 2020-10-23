c7ce6ea70c22221460f2b01611ce6b5a9e055da1
David Eynon <davideynon@Davids-MacBook-Pro.local>
2020-10-23 18:11:57 +0200

    Populated counter with built-in Counter instantiation instead of manual population. (shows good knowledge of Counter to interviewer and slight optimization).

--- Chapter1/2_Check Permutation/CheckPermutation.py
+++ Chapter1/2_Check Permutation/CheckPermutation.py
@@ -6,9 +6,7 @@ from collections import Counter
 def check_permutation(str1, str2):
     if len(str1) != len(str2):
         return False
-    counter = Counter()
-    for c in str1:
-        counter[c] += 1
+    counter = Counter(str1)
     for c in str2:
         if counter[c] == 0:
             return False

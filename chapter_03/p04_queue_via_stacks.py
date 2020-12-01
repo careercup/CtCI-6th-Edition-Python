d443c37d1a40c55dd59d09a33a990144255bc539
Mark Ryan <markryandeveloper@gmail.com>
2020-11-30 20:15:42 -0500

    test(C03_P04): added a few basic tests

--- chapter_03/p04_queue_via_stacks.py
+++ chapter_03/p04_queue_via_stacks.py
@@ -37,13 +37,63 @@ class MyQueue:
 
 
 class Tests(unittest.TestCase):
+    test_cases = [([1, 2, 3]), ([-1, 0, 1]), (["a", "b", "c", "d", "e", "f"])]
+
+    def test_size(self):
+        for vals in self.test_cases:
+            q = MyQueue()
+            for index, val in enumerate(vals, start=1):
+                q.add(val)
+                self.assertEqual(len(q), index)
+            for index, val in enumerate(vals, start=1):
+                q.remove()
+                self.assertEqual(len(q), len(vals) - index)
+
+    def test_add(self):
+        for vals in self.test_cases:
+            q = MyQueue()
+            for val in vals:
+                q.add(val)
+            self.assertEqual(q.peek(), vals[0])
+            self.assertEqual(len(q), len(vals))
+
+    def test_shift_stacks(self):
+        for vals in self.test_cases:
+            q = MyQueue()
+            for val in vals:
+                q.add(val)
+            self.assertEqual(len(q.old_stack), 0)
+            self.assertEqual(len(q.new_stack), len(vals))
+            self.assertEqual(q.new_stack.peek(), vals[-1])
+            q._shift_stacks()
+            self.assertEqual(len(q.old_stack), len(vals))
+            self.assertEqual(len(q.new_stack), 0)
+            self.assertEqual(q.old_stack.peek(), vals[0])
+
     def test_peek(self):
+        for vals in self.test_cases:
+            q = MyQueue()
+            for val in vals:
+                q.add(val)
+                self.assertEqual(q.peek(), vals[0])
+            q.remove()
+            self.assertEqual(q.peek(), vals[1])
+
+    def test_remove(self):
+        for vals in self.test_cases:
+            q = MyQueue()
+            for val in vals:
+                q.add(val)
+            for i in range(len(vals)):
+                self.assertEqual(q.remove(), vals[i])
+
+    def test_peek_simple(self):
         q = MyQueue()
         q.add(4)
         q.add(6)
         self.assertEqual(q.peek(), 4)
 
-    def test_add(self):
+    def test_add_simple(self):
         q = MyQueue()
         q.add(4)
         q.add(6)
@@ -51,7 +101,7 @@ class Tests(unittest.TestCase):
         q.add(101)
         self.assertNotEqual(q.peek(), 101)
 
-    def test_remove(self):
+    def test_remove_simple(self):
         q = MyQueue()
         q.add(4)
         q.add(6)

395ee6b83c1c58191b0b3e794ac62f81f46498ed
Bryce <gpg-2019-08-19@brycedrennan.com>
2020-12-03 21:56:06 -0800

    fix: urlify_stdlib had wrong function signature

--- Chapter01/3_URLify/URLify.py
+++ Chapter01/3_URLify/URLify.py
@@ -2,17 +2,17 @@
 import unittest
 
 
-def urlify(string, length):
-    '''function replaces single spaces with %20 and removes trailing spaces'''
+def urlify_algo(string, length):
+    """replace single spaces with %20 and removes trailing spaces"""
     # convert to list because Python strings are immutable
     char_list = list(string)
     string = ""
     new_index = len(char_list)
 
     for i in reversed(range(length)):
-        if char_list[i] == ' ':
+        if char_list[i] == " ":
             # Replace spaces
-            char_list[new_index - 3:new_index] = '%20'
+            char_list[new_index - 3 : new_index] = "%20"
             new_index -= 3
         else:
             # Move characters
@@ -22,25 +22,25 @@ def urlify(string, length):
     return string.join(char_list)
 
 
-def urlify_stdlib(text):
+def urlify_stdlib(text, length):
     """solution using standard library"""
-    text = text.rstrip()
-    url = text.replace(" ", "%20")
-    return url
+    return text.rstrip().replace(" ", "%20")
 
 
 class Test(unittest.TestCase):
-    '''Test Cases'''
-    data = [
-        ('much ado about nothing      ', 22,
-         'much%20ado%20about%20nothing'),
-        ('Mr John Smith    ', 13, 'Mr%20John%20Smith')]
+    """Test Cases"""
+
+    test_cases = [
+        ("much ado about nothing      ", 22, "much%20ado%20about%20nothing"),
+        ("Mr John Smith    ", 13, "Mr%20John%20Smith"),
+    ]
 
     def test_urlify(self):
-        for func in (urlify, urlify_stdlib):
-            for [test_string, length, expected] in self.data:
+        for func in (urlify_algo, urlify_stdlib):
+            for test_string, length, expected in self.test_cases:
                 actual = func(test_string, length)
                 self.assertEqual(actual, expected)
+
 
 if __name__ == "__main__":
     unittest.main()

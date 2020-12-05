979d9b7a4223b9517468bd61275ff7cb453a0de0
Bryce <gpg-2019-08-19@brycedrennan.com>
2020-12-05 00:56:27 -0800

    docs: add warning about doubly nested arrays for C01_P01

--- Chapter01/p01_is_unique.py
+++ Chapter01/p01_is_unique.py
@@ -7,6 +7,8 @@ def is_unique_chars_algorithmic(string):
     if len(string) > 128:
         return False
 
+    # this is a pythonic and faster way to initialize an array with a fixed value. careful though
+    # it won't work for a doubly nested array
     char_set = [False] * 128
     for char in string:
         val = ord(char)

10d98c313a95975c97468fc0dfc0b7882836bbb9
Bryce <gpg-2019-08-19@brycedrennan.com>
2020-12-15 19:20:52 -0800

    test(C02_P06): add tests

--- chapter_02/p06_palindrome.py
+++ chapter_02/p06_palindrome.py
@@ -1,3 +1,5 @@
+import time
+
 from chapter_02.linked_list import LinkedList
 
 
@@ -24,8 +26,30 @@ def is_palindrome(ll):
     return True
 
 
+test_cases = [
+    ([1, 2, 3, 4, 3, 2, 1], True),
+    ([1], True),
+    (["a", "a"], True),
+    ("aba", True),
+    ([], True),
+    ([1, 2, 3, 4, 5], False),
+    ([1, 2], False),
+]
+
+testable_functions = [is_palindrome]
+
+
 def test_is_palindrome():
-    ll_true = LinkedList([1, 2, 3, 4, 5, 4, 3, 2, 1])
-    assert is_palindrome(ll_true)
-    ll_false = LinkedList([1, 2, 3, 4, 5, 6, 7, 8, 9])
-    assert not is_palindrome(ll_false)
+    for f in testable_functions:
+        start = time.perf_counter()
+        for values, expected in test_cases:
+            print(f"{f.__name__}: {values}")
+            for _ in range(100):
+                assert f(LinkedList(values)) == expected
+
+        duration = time.perf_counter() - start
+        print(f"{f.__name__} {duration * 1000:.1f}ms")
+
+
+if __name__ == "__main__":
+    test_is_palindrome()

9f40c63d88c0360bbdfcdd6f8706186f03649a4c
marimo <43314030+marimoon@users.noreply.github.com>
2021-01-04 09:49:56 +0900

    fix(C01_P01): add more test cases (#140)

--- chapter_01/p01_is_unique.py
+++ chapter_01/p01_is_unique.py
@@ -31,6 +31,8 @@ class Test(unittest.TestCase):
         ("", True),
         ("23ds2", False),
         ("hb 627jh=j ()", False),
+        ("".join([chr(val) for val in range(128)]), True),  # unique 128 chars
+        ("".join([chr(val // 2) for val in range(129)]), False),  # non-unique 129 chars
     ]
     test_functions = [is_unique_chars_pythonic, is_unique_chars_algorithmic]
 

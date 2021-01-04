a3239ce6c4169808eaff4b320103cef28ea26dbe
Amey Naik <ameynaik@Admins-MacBook-Pro.local>
2021-01-04 13:10:06 -0800

    feature(C04_P06): added tests

--- chapter_04/p06_successor.py
+++ chapter_04/p06_successor.py
@@ -29,14 +29,16 @@ if __name__ == "__main__":
     bst.insert(11)
     bst.insert(14)
 
-    # Get a reference to the node whose key is 9
-    test = bst.get_node(12)
 
-    # Find the in order successor of test
-    succ = in_order_successor(test)
+    # Test all nodes
+    inputs = [5,9,11,12,14,20,25]
+    outputs = inputs[1:]
+    outputs.append(None)
 
-    # Print the key of the successor node
-    if succ is not None:
-        print("\nInorder Successor of %d is %d " % (test.key, succ.key))
-    else:
-        print("\nInorder Successor doesn't exist")
+    for x,y in zip(inputs,outputs):
+        test = bst.get_node(x)
+        succ = in_order_successor(test)
+        if succ is not None:
+            assert(succ.key == y)
+        else:
+            assert(succ == y)

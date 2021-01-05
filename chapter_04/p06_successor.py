0524a6869745396ca618dd41edf04e4544508689
Amey Naik <ameynaik@Admins-MacBook-Pro.local>
2021-01-04 17:16:55 -0800

    feature(C04_P06): added tests; with clean lint

--- chapter_04/p06_successor.py
+++ chapter_04/p06_successor.py
@@ -29,16 +29,15 @@ if __name__ == "__main__":
     bst.insert(11)
     bst.insert(14)
 
-
     # Test all nodes
-    inputs = [5,9,11,12,14,20,25]
+    inputs = [5, 9, 11, 12, 14, 20, 25]
     outputs = inputs[1:]
     outputs.append(None)
 
-    for x,y in zip(inputs,outputs):
+    for x, y in zip(inputs, outputs):
         test = bst.get_node(x)
         succ = in_order_successor(test)
         if succ is not None:
-            assert(succ.key == y)
+            assert succ.key == y
         else:
-            assert(succ == y)
+            assert succ == y

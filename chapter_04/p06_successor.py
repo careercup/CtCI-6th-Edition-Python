975a500a7de0a87a0b11cac8e73b473cf4651fb0
WhimsicalCoder <ameynaik1989@gmail.com>
2021-01-04 19:17:26 -0800

    feature(C04_P06): added tests (#141)
    feature(C04_P06): added tests

--- chapter_04/p06_successor.py
+++ chapter_04/p06_successor.py
@@ -19,7 +19,7 @@ def in_order_successor(input_node):
     return ancestor
 
 
-if __name__ == "__main__":
+def test_in_order_successor():
     bst = BinarySearchTree()
     bst.insert(20)
     bst.insert(9)
@@ -29,14 +29,19 @@ if __name__ == "__main__":
     bst.insert(11)
     bst.insert(14)
 
-    # Get a reference to the node whose key is 9
-    test = bst.get_node(12)
+    # Test all nodes
+    inputs = [5, 9, 11, 12, 14, 20, 25]
+    outputs = inputs[1:]
+    outputs.append(None)
 
-    # Find the in order successor of test
-    succ = in_order_successor(test)
+    for x, y in zip(inputs, outputs):
+        test = bst.get_node(x)
+        succ = in_order_successor(test)
+        if succ is not None:
+            assert succ.key == y
+        else:
+            assert succ == y
 
-    # Print the key of the successor node
-    if succ is not None:
-        print("\nInorder Successor of %d is %d " % (test.key, succ.key))
-    else:
-        print("\nInorder Successor doesn't exist")
+
+if __name__ == "__main__":
+    test_in_order_successor()

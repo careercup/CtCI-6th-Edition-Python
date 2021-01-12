eadd0d0ce7742a7eef3f818c0591e5c51c015a07
Barış Sarı <bayrambariss@gmail.com>
2021-01-12 11:35:25 +0300

    update(C01_P01): added bitwise solution (#150)

--- chapter_01/p01_is_unique.py
+++ chapter_01/p01_is_unique.py
@@ -24,6 +24,21 @@ def is_unique_chars_pythonic(string):
     return len(set(string)) == len(string)
 
 
+def is_unique_bit_vector(string):
+    """Uses bitwise operation instead of extra data structures."""
+    # Assuming character set is ASCII (128 characters)
+    if len(string) > 128:
+        return False
+
+    checker = 0
+    for c in string:
+        val = ord(c)
+        if (checker & (1 << val)) > 0:
+            return False
+        checker |= 1 << val
+    return True
+
+
 class Test(unittest.TestCase):
     test_cases = [
         ("abcd", True),
@@ -34,7 +49,11 @@ class Test(unittest.TestCase):
         ("".join([chr(val) for val in range(128)]), True),  # unique 128 chars
         ("".join([chr(val // 2) for val in range(129)]), False),  # non-unique 129 chars
     ]
-    test_functions = [is_unique_chars_pythonic, is_unique_chars_algorithmic]
+    test_functions = [
+        is_unique_chars_pythonic,
+        is_unique_chars_algorithmic,
+        is_unique_bit_vector,
+    ]
 
     def test_is_unique_chars(self):
         for is_unique_chars in self.test_functions:

10720f8dd568538ec00dad32ccc0b9ef9f43c32f
Bryce <gpg-2019-08-19@brycedrennan.com>
2021-02-16 09:33:37 -0800

    test(C08_P04): add test

--- chapter_08/p04_power_set.py
+++ chapter_08/p04_power_set.py
@@ -2,13 +2,13 @@ import copy
 
 
 # Solution using recursion
-def get_subsets(setz, index=None):
+def get_subsets_a(setz, index=None):
     if index is None:
         index = len(setz) - 1
     if index == -1:
         return [[]]
 
-    old_subs = get_subsets(setz, index - 1)
+    old_subs = get_subsets_a(setz, index - 1)
     new_subs = []
     item = setz[index]
     for val in old_subs:
@@ -22,7 +22,7 @@ def get_subsets(setz, index=None):
 
 
 # Combinatorics Solution
-def get_subsets_2(aset):
+def get_subsets_b(aset):
     all_subsets = []
     max_n = 1 << len(aset)
     for k in range(max_n):
@@ -43,7 +43,20 @@ def convert_int_to_set(x, aset):
     return subset
 
 
+testable_functions = [get_subsets_a, get_subsets_b]
+
+test_cases = [({1, 2, 3}, {(), (1,), (1, 2), (1, 2, 3), (1, 3), (2,), (2, 3), (3,)})]
+
+
+def test_get_subsets():
+    for input_set, expected in test_cases:
+        for get_subsets in testable_functions:
+            results = get_subsets(list(input_set))
+            results = {tuple(s) for s in results}
+            assert results == expected
+
+
 if __name__ == "__main__":
-    print(get_subsets([1, 2, 3]))
+    print(get_subsets_a([1, 2, 3]))
     print("")
-    print(get_subsets_2([1, 2, 3]))
+    print(get_subsets_b([1, 2, 3]))

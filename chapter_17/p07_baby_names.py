af2da2bee710b41117c7f58f5ec0bcbf63426e83
Rajat Modi <rajatmodi@Rajats-MacBook-Pro.local>
2021-03-16 14:11:06 +0530

    C17_P07):add solution

--- chapter_17/p07_baby_names.py
+++ chapter_17/p07_baby_names.py
@@ -1,41 +1,50 @@
 import collections
 
 names = {
-		"john":10,\
-		"jon":3,\
-		"davis":2,\
-		"kari":3,\
-		"johny":11,\
-		"carlton":8 ,\
-		"carleton":2,\
-		"jonathan":9,\
-		"carrie":5
-
+    "john": 10,
+    "jon": 3,
+    "davis": 2,
+    "kari": 3,
+    "johny": 11,
+    "carlton": 8,
+    "carleton": 2,
+    "jonathan": 9,
+    "carrie": 5,
 }
 
-pairs = [["jonathan","john"],["jon","johny"],["johny","john"],["kari","carrie"],["carleton","carlton"]]
+pairs = [
+    ["jonathan", "john"],
+    ["jon", "johny"],
+    ["johny", "john"],
+    ["kari", "carrie"],
+    ["carleton", "carlton"],
+]
 
 parent = {}
 for name in names.keys():
-	parent[name] = name
+    parent[name] = name
 
-#find operation
+# find operation
 def find(x):
-	if parent[x]!=x:
-		parent[x]=find(parent[x])
-	return parent[x]
-#union operation
-def union(x1,x2):
-	r1 = find(x1)
-	r2 = find(x2)
-	if r1!=r2:
-		parent[r1]=r2
+    if parent[x] != x:
+        parent[x] = find(parent[x])
+    return parent[x]
+
+
+# union operation
+def union(x1, x2):
+    r1 = find(x1)
+    r2 = find(x2)
+    if r1 != r2:
+        parent[r1] = r2
+
+
 res = collections.defaultdict(int)
 for pair in pairs:
-	union(pair[0],pair[1])
+    union(pair[0], pair[1])
 
 for key in parent.keys():
-	#find root of cluster
-	root = find(key)
-	res[root]+=names[key]
+    # find root of cluster
+    root = find(key)
+    res[root] += names[key]
 print(res)

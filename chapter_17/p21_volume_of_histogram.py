da2ce767ac4259e91ac29de5af4e495d654727f5
Rajat Modi <rajatmodi@rajats-macbook-pro.local>
2021-03-18 16:48:29 +0530

    feature(C17_P21): add solution

---
+++ chapter_17/p21_volume_of_histogram.py
@@ -0,0 +1,24 @@
+#volume of histogram
+#o(n) time
+#o(n) space
+
+hist = [0,0,4,0,0,6,0,0,3,0,5,0,1,0,0,0]
+def find_volume(a):
+    if len(a)<=2:
+        return 0
+    n = len(a)
+    h_left = [0]*n #max height on left, excluding the curr bar
+    h_right = [0]*n #max height on right, excluding the curr bar
+    for i in range(1,n):
+        h_left[i]= max(h_left[i-1],a[i-1])
+    for i in reversed(range(n-1)):
+        h_right[i]= max(h_right[i+1],a[i+1])
+    res = 0
+    #neglect extreme left and extreme right bar, they can never be filled
+    for i in range(1,n-1):
+        min_height = min(h_left[i],h_right[i])
+        if min_height-a[i]>0:
+            res+=min_height-a[i]
+    print(res)
+    return res
+find_volume(hist)

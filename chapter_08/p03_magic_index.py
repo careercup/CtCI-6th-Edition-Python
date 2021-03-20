2eb0fabdebfe16f73a4cbaa8cb47cf00cea33b77
Parin Shah <parinpshah94@gmail.com>
2021-03-20 10:01:10 -0700

    feature(C08_P03): added solution to the follow-up

--- chapter_08/p03_magic_index.py
+++ chapter_08/p03_magic_index.py
@@ -1,8 +1,4 @@
-import pytest
-
-
-class NoMagicIndexExistsError(RuntimeError):
-    pass
+NOT_FOUND = -1
 
 
 def magic_index(array, min_index=0, max_index=None):
@@ -10,7 +6,7 @@ def magic_index(array, min_index=0, max_index=None):
         max_index = len(array) - 1
 
     if max_index < min_index:
-        raise NoMagicIndexExistsError("No magic index exists")
+        return NOT_FOUND
 
     mid = (max_index + min_index) // 2
     if array[mid] == mid:
@@ -21,31 +17,50 @@ def magic_index(array, min_index=0, max_index=None):
         return magic_index(array, min_index, mid - 1)
 
 
+def magic_index_non_distinct(array, min_index=0, max_index=None):
+    if max_index is None:
+        max_index = len(array) - 1
+
+    if max_index < min_index:
+        return NOT_FOUND
+
+    mid = (max_index + min_index) // 2
+    if array[mid] == mid:
+        return mid
+
+    # Search left recursively
+    left_index = min(mid - 1, array[mid])
+    left = magic_index_non_distinct(array, min_index, left_index)
+    if left >= 0:
+        return left
+
+    # Search right recursively
+    right_index = max(mid + 1, array[mid])
+    return magic_index_non_distinct(array, right_index, max_index)
+
+
 test_cases = [
     ([-14, -12, 0, 1, 2, 5, 9, 10, 23, 25], 5),
-    ([-14, -12, 0, 1, 2, 7, 9, 10, 23, 25], NoMagicIndexExistsError),
+    ([-14, -12, 0, 1, 2, 7, 9, 10, 23, 25], NOT_FOUND),
     ([0, 1, 2, 3, 4], 2),
-    ([], NoMagicIndexExistsError),
+    ([], NOT_FOUND),
 ]
 
-followup_test_cases = [
-    ([-10, -5, 2, 2, 2, 3, 4, 7, 9, 12, 13], 3),
+followup_test_cases = test_cases + [
+    ([-10, -5, 2, 2, 2, 3, 4, 7, 9, 12, 13], 2),
 ]
 
 
 def test_magic_index():
     for array, expected in test_cases:
-        if isinstance(expected, int):
-            assert magic_index(array) == expected
-        else:
-            with pytest.raises(expected):
-                magic_index(array)
+        assert magic_index(array) == expected
 
 
-def example():
-    array = [-14, -12, 0, 1, 2, 5, 9, 10, 23, 25]
-    print(magic_index(array, 0, len(array) - 1))
+def test_magic_index_non_distinct():
+    for array, expected in followup_test_cases:
+        assert magic_index_non_distinct(array) == expected
 
 
 if __name__ == "__main__":
-    example()
+    test_magic_index()
+    test_magic_index_non_distinct()

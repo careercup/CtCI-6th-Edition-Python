00d28501e78d9e530138107dfa8f0e9dfd91b44a
Parin Shah <parinpshah94@gmail.com>
2021-03-20 10:01:10 -0700

    feature(C08_P03): added solution to the follow-up

--- chapter_08/p03_magic_index.py
+++ chapter_08/p03_magic_index.py
@@ -10,10 +10,37 @@ def magic_index(array, min_index, max_index):
     raise Exception("Not possible")
 
 
-def example():
+def magic_index_non_distinct(array, min_index, max_index):
+    if max_index < min_index:
+        return -1
+
+    mid = (max_index + min_index) // 2
+    if array[mid] == mid:
+        return mid
+
+    # Search left recursively
+    left_index = min(mid - 1, array[mid])
+    left = magic_index_non_distinct(array, min_index, left_index)
+    if left >= 0:
+        return left
+
+    # Search right recursively
+    right_index = max(mid + 1, array[mid])
+    right = magic_index_non_distinct(array, right_index, max_index)
+    if right >= 0:
+        return right
+
+
+def example_magic_index():
     array = [-14, -12, 0, 1, 2, 5, 9, 10, 23, 25]
     print(magic_index(array, 0, len(array) - 1))
 
 
+def example_magic_index_non_distinct():
+    array = [-10, -5, 1, 3, 3, 3, 4, 7, 9, 12, 13]
+    print(magic_index_non_distinct(array, 0, len(array) - 1))
+
+
 if __name__ == "__main__":
-    example()
+    example_magic_index()
+    example_magic_index_non_distinct()

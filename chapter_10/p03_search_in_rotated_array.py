1142a56bf54c0249dbf5191514e105fbc2f09a69
NicoJ <nicolasj@outlook.com>
2021-03-21 15:23:36 -0400

    feature(C10_P03): alternative recursive option

--- chapter_10/p03_search_in_rotated_array.py
+++ chapter_10/p03_search_in_rotated_array.py
@@ -30,6 +30,40 @@ def index(nums: Sequence[int], target: int) -> Optional[int]:
     return None
 
 
+def search_rotated(array: Sequence[int], num: int) -> Optional[int]:
+    if not array:
+        return None
+    return _recursive_search(array, num, 0, len(array) - 1)
+
+
+def _recursive_search(array, num, start, end):
+    middle = (end - start) // 2 + start
+    if array[middle] == num:
+        return middle
+    if end - start <= 0:
+        return None
+
+    result = None
+    if array[start] < array[middle]:  # left side is normal
+        if array[start] <= num < array[middle]:
+            result = _recursive_search(array, num, start, middle - 1)
+        else:
+            result = _recursive_search(array, num, middle + 1, end)
+    elif array[middle] < array[end]:  # right side is normal
+        if array[middle] < num <= array[end]:
+            result = _recursive_search(array, num, middle + 1, end)
+        else:
+            result = _recursive_search(array, num, start, middle - 1)
+    elif array[start] == array[middle]:
+        if array[middle] != array[end]:
+            result = _recursive_search(array, num, middle + 1, end)
+        else:
+            result = _recursive_search(array, num, start, middle - 1)
+            if result is None:
+                result = _recursive_search(array, num, middle + 1, end)
+    return result
+
+
 test_cases = [
     # array, target, valid solutions
     ([15, 16, 19, 20, 25, 1, 3, 4, 5, 7, 10, 14], 5, 8),
@@ -40,14 +74,21 @@ test_cases = [
     ([2, 3, 1, 2, 2, 2, 2, 2, 2, 2], 8, None),
 ]
 
+testable_functions = [index, search_rotated]
+
 
 def test_index():
     for array, target, expected in test_cases:
-        ind = index(array, target)
-        if isinstance(expected, set):
-            assert ind in expected
-        else:
-            error_msg = (
-                f"array:{array} target:{target} calculated:{ind} expected:{expected}"
-            )
-            assert ind == expected, error_msg
+        for method in testable_functions:
+            ind = method(array, target)
+            if isinstance(expected, set):
+                assert ind in expected
+            else:
+                error_msg = (
+                    f"arr:{array} target:{target} calculated:{ind} expected:{expected}"
+                )
+                assert ind == expected, error_msg
+
+
+if __name__ == "__main__":
+    test_index()

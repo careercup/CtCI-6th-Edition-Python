36e62dd091ac1ebd25e70db505ebf702f47b4a54
Bryce <gpg-2019-08-19@brycedrennan.com>
2021-05-07 23:09:53 -0700

    refactor(C17_P17): cleanup

--- chapter_17/p17_multi_search.py
+++ chapter_17/p17_multi_search.py
@@ -1,48 +1,60 @@
-#trie solution
-#third optimal solution in ctci
-#o(kt+bk)
+# trie solution
+# third optimal solution in ctci
+# o(kt+bk)
 import collections
-class trienode:
+
+
+class TrieNode:
     def __init__(self):
-        self.children=collections.defaultdict(trienode)
+        self.children = collections.defaultdict(TrieNode)
         self.is_word = False
 
-class trie:
+
+class Trie:
     def __init__(self):
-        self.root = trienode()
-    def insert(self,word):
+        self.root = TrieNode()
+
+    def insert(self, word):
         node = self.root
         for letter in word:
             node = node.children[letter]
         node.is_word = True
-    def check_existence(self,word):
-        #checks if the prefixes exist on a path,
-        #if they do, then return the location in the word
+
+    def check_existence(self, word):
+        # checks if the prefixes exist on a path,
+        # if they do, then return the location in the word
         res = []
         node = self.root
-        for i,letter in enumerate(word):
+        for i, letter in enumerate(word):
             if letter in node.children.keys():
                 node = node.children[letter]
                 if node.is_word:
-                    res.append(word[0:i+1])
+                    res.append(word[0 : i + 1])
             else:
                 break
         return res
 
-small_words = ['i','is','pp','ms']
-s = 'mississippi'
-t = trie()
 
-#insert t's in trie
-for word in small_words:
-    t.insert(word)
-#a dict with t's element as keys, and all the possible indices where they are discovered in string
-res = collections.defaultdict(list)
-#start matching the sentence now
-n = len(s)
-for i in range(n):
-    discovered_words = t.check_existence(s[i:])
-    for word in discovered_words:
-        #the discovered word, starts at i
-        res[word].append(i)
-print(res)
+def multisearch(text, search_terms):
+    t = Trie()
+
+    # insert t's in trie
+    for word in search_terms:
+        t.insert(word)
+    # a dict with t's element as keys, and all the possible indices
+    # where they are discovered in string
+    res = collections.defaultdict(list)
+    # start matching the sentence now
+    n = len(text)
+    for i in range(n):
+        discovered_words = t.check_existence(text[i:])
+        for word in discovered_words:
+            # the discovered word, starts at i
+            res[word].append(i)
+    return dict(res)
+
+
+def test_multisearch():
+    small_words = ["i", "is", "pp", "ms"]
+    expected = {"i": [1, 4, 7, 10], "is": [1, 4], "pp": [8]}
+    assert multisearch("mississippi", small_words) == expected

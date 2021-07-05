287351ca446b8b5f2157b0e2a141b2cedf85bae4
PBearson <peabryan95@gmail.com>
2021-07-05 17:40:53 -0400

    Fix lit with flake8 checks

--- chapter_04/p04_check_balanced.py
+++ chapter_04/p04_check_balanced.py
@@ -4,62 +4,66 @@ class BinaryNode:
         self.left = None
         self.right = None
 
-# Version 2: 
+
+# Version 2:
 # Traverse the tree and track the largest and smallest depth of each leaf node.
 # Then compare the largest and smallest depth.
 def is_balanced_v2(node):
-    minDepth = 10 ** 100
-    maxDepth = -10 ** 100
+    min_depth = 10 ** 100
+    max_depth = -10 ** 100
     queue = [(node, 0)]
     visited = [node]
 
     while len(queue) > 0:
-        currNode, currDepth = queue.pop(0)
+        curr_node, curr_depth = queue.pop(0)
 
-        if currNode.left is None and currNode.right is None:
-            if currDepth > maxDepth:
-                maxDepth = currDepth
-            if currDepth < minDepth:
-                minDepth = currDepth
+        if curr_node.left is None and curr_node.right is None:
+            if curr_depth > max_depth:
+                maxDepth = curr_depth
+            if curr_depth < min_depth:
+                min_depth = curr_depth
         else:
-            if currNode.left and currNode.left not in visited:
-                visited.append(currNode.left)
-                queue.append((currNode.left, currDepth + 1))
-            if currNode.right and currNode.right not in visited:
-                visited.append(currNode.right)
-                queue.append((currNode.right, currDepth + 1))
+            if curr_node.left and curr_node.left not in visited:
+                visited.append(curr_node.left)
+                queue.append((curr_node.left, curr_depth + 1))
+            if curr_node.right and curr_node.right not in visited:
+                visited.append(curr_node.right)
+                queue.append((curr_node.right, curr_depth + 1))
 
-    return maxDepth - minDepth < 2
-        
+    return maxDepth - min_depth < 2
 
-def findMaxDepth(node, level = 0):
+
+def find_max_depth(node, level=0):
     if node is None:
         return level
     if not node.left:
-        return findMaxDepth(node.right, level + 1)
+        return find_max_depth(node.right, level + 1)
     if not node.right:
-        return findMaxDepth(node.left, level + 1)
-    return max(findMaxDepth(node.left, level + 1), findMaxDepth(node.right, level + 1))
+        return find_max_depth(node.left, level + 1)
+    return max(find_max_depth(node.left, level + 1), find_max_depth(node.right, level + 1))
 
-def findMinDepth(node, level = 0):
+
+def find_min_depth(node, level=0):
     if node is None:
         return level
     if not node.left:
-        return findMinDepth(node.right, level + 1)
+        return find_min_depth(node.right, level + 1)
     if not node.right:
-        return findMinDepth(node.left, level + 1)
-    return min(findMinDepth(node.left, level + 1), findMinDepth(node.right, level + 1))
+        return find_min_depth(node.left, level + 1)
+    return min(find_min_depth(node.left, level + 1), find_min_depth(node.right, level + 1))
+
 
 # Version 1:
 # Find the max tree depth and min tree depth independently.
 # Then compare their values.
 def is_balanced_v1(node):
-    return findMaxDepth(node) - findMinDepth(node) < 2
+    return find_max_depth(node) - find_min_depth(node) < 2
+
 
 def example():
     root = BinaryNode(1)
     root.left = BinaryNode(2)
-    assert is_balanced_v1(root) == is_balanced_v2(root) == True
+    assert is_balanced_v1(root) == is_balanced_v2(root) is True
 
     root.left.left = BinaryNode(4)
     root.left.right = BinaryNode(5)
@@ -70,7 +74,7 @@ def example():
     root.right.right = BinaryNode(9)
     root.right.right.right = BinaryNode(10)
     root.right.right.right.right = BinaryNode(11)
-    assert is_balanced_v1(root) == is_balanced_v2(root) == False
+    assert is_balanced_v1(root) == is_balanced_v2(root) is False
 
     tree = BinaryNode(1)
     tree.left = BinaryNode(2)
@@ -83,7 +87,7 @@ def example():
     tree.left.right.left = BinaryNode(12)
     tree.left.right.left.left = BinaryNode(16)
     tree.left.right.left.right = BinaryNode(0)
-    assert is_balanced_v1(tree) == is_balanced_v2(tree) == False
+    assert is_balanced_v1(tree) == is_balanced_v2(tree) is False
 
     root = BinaryNode(7)
     root.left = BinaryNode(2)
@@ -92,7 +96,7 @@ def example():
     root.right.left = BinaryNode(8)
     root.right.right = BinaryNode(9)
     root.right.right.right = BinaryNode(10)
-    assert is_balanced_v1(root) == is_balanced_v2(root) == True
+    assert is_balanced_v1(root) == is_balanced_v2(root) is True
 
 
 if __name__ == "__main__":

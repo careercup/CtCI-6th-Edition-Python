8e99122e743bfb83b26da9f6b1e0f0abc91320ef
Kenneth Auyeung <kennethauyeung1@gmail.com>
2022-04-15 15:58:11 -0700

    refactor(C02_P02): use one loop

--- chapter_02/p02_return_kth_to_last.py
+++ chapter_02/p02_return_kth_to_last.py
@@ -2,17 +2,15 @@ from chapter_02.linked_list import LinkedList
 
 
 def kth_to_last(ll, k):
-    runner = current = ll.head
-    for _ in range(k):
-        if not runner:
-            return None
-        runner = runner.next
+    leader = follower = ll.head
+    count = 0
 
-    while runner:
-        current = current.next
-        runner = runner.next
-
-    return current
+    while leader:
+        if count >= k:
+            follower = follower.next
+        count += 1
+        leader = leader.next
+    return follower
 
 
 # O(N) space

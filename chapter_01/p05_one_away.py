c734bfde1330aa4e2433586db653595903bdb012
Mitra Mirshafiee <53291220+mitramir55@users.noreply.github.com>
2022-10-08 10:58:08 -0600

    a shorter and cleaner code
    I believe this also works with O(n), but is shorter and more understandable.

--- chapter_01/p05_one_away.py
+++ chapter_01/p05_one_away.py
@@ -4,38 +4,22 @@ import unittest
 
 
 def are_one_edit_different(s1, s2):
-    """Check if a string can converted to another string with a single edit"""
-    if len(s1) == len(s2):
-        return one_edit_replace(s1, s2)
-    if len(s1) + 1 == len(s2):
-        return one_edit_insert(s1, s2)
-    if len(s1) - 1 == len(s2):
-        return one_edit_insert(s2, s1)  # noqa
-    return False
+    """
+    Check if a string can converted to another string with a single edit.
+    cat -> mat : True
+    cat -> matt: False
+    """
+    if abs(len(s1) - len(s2)) > 1: return False
+    char_map = [0] * 128
+    for k in s1:
+        idx = ord(k)
+        char_map[idx] += 1
+    for k in s2:
+        idx = ord(k)
+        char_map[idx] -= 1
 
-
-def one_edit_replace(s1, s2):
-    edited = False
-    for c1, c2 in zip(s1, s2):
-        if c1 != c2:
-            if edited:
-                return False
-            edited = True
-    return True
-
-
-def one_edit_insert(s1, s2):
-    edited = False
-    i, j = 0, 0
-    while i < len(s1) and j < len(s2):
-        if s1[i] != s2[j]:
-            if edited:
-                return False
-            edited = True
-            j += 1
-        else:
-            i += 1
-            j += 1
+    # more than two because we're ok with each having one different letter
+    if sum(abs(i) for i in char_map) > 2: return False
     return True
 
 
